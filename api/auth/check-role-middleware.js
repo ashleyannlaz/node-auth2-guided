e93feb621d3348c6f27ff105af1d480eb88d1959
Dave Kidd <dave@davekidd.net>
2021-09-02 19:00:26 -0600

    wip

--- api/auth/check-role-middleware.js
+++ api/auth/check-role-middleware.js
@@ -2,4 +2,7 @@ module.exports = (role)=>(req,res,next)=>{
     if(req.decodedToken.role === role){
         next()
     }
+    else{
+        res.status(403).json("You need to be an admin")
+    }
 }
