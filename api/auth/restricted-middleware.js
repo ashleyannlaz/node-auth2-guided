21a80f60b3b4fea3d11a30b538daf3c4943c24e9
Dave Kidd <dave@davekidd.net>
2021-09-02 18:31:43 -0600

    wip

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -1,5 +1,11 @@
+const { json } = require("express");
+
 module.exports = (req, res, next) => {
   const token = req.headers.authorization //this pulls the token from the req
-
+  if(!token){
+    res.status(401).json("Token please!")
+  }else{
+    
+  }
 
 };
