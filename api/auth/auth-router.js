c4e846d2f595c9bcb6babd959185c181059ad552
Dave Kidd <dave@davekidd.net>
2021-09-02 19:11:38 -0600

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken")
 const router = require('express').Router();
+const {jwtSecret} = require("../../config/secrets.js")
 
 const Users = require('../users/users-model.js');
 const checkAuthPayload = require('./check-payload-middleware');
