9c3e00bed0f01690efe19a41ffb471b91d6221be
Dave Kidd <dave@davekidd.net>
2021-09-02 18:32:48 -0600

    wip

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -1,11 +1,11 @@
-const { json } = require("express");
+const jwt = require("jsonwebtoken")
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization //this pulls the token from the req
   if(!token){
     res.status(401).json("Token please!")
   }else{
-    
+
   }
 
 };
