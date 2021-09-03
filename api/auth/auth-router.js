58a6dbd17eaa546c697eb585533b0b5a4b9799e4
Dave Kidd <dave@davekidd.net>
2021-09-02 18:13:04 -0600

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -28,6 +28,7 @@ router.post('/login', checkAuthPayload, (req, res, next) => {
   Users.findBy({ username }) // it would be nice to have middleware do this
     .then(([user]) => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = makeToken(user)
         res.status(200).json({
           message: `Welcome back ${user.username}!`,
         });
